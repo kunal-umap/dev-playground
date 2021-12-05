890d6f3aa7fbc087f846606ddedc90777bbeff1a
Kunal Umap <80446986+kunal-umap@users.noreply.github.com>
2021-12-05 17:01:07 +0530

    added styled Component

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import styled from 'styled-component';
+import styled from 'styled-components';
 
 const Container = styled.div`
     height: 100vh;
-    
+    color: blue;
 `;
 
 export default function Login() {
     return (
         <Container>
-            
+            <h1>Hello World</h1>
         </Container>
     )
 }
