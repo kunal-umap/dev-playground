7a6727a9da3f5635c23dceccd80ba70645a12158
Kunal Umap <80446986+kunal-umap@users.noreply.github.com>
2021-12-05 16:52:02 +0530

    creted folder stricture again

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-component';
+
+const Container = styled.div`
+    height: 100vh;
+    
+`;
+
+export default function Login() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
